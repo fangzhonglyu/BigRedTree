1a5214b8c01045753687f93393d82accc9ff5461
fl327 <soxehli@gmail.com>
2022-10-14 21:27:07 -0400

    Add baseline version of server backend

--- src/app.py
+++ src/app.py
@@ -1,11 +1,11 @@
 from calendar import c
-from db import db, Course, Assignments, Users
+from db import db, Users
 from flask import Flask, request
 import json
 import os
 
 app = Flask(__name__)
-db_filename = "cms.db"
+db_filename = "tree.db"
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -13,148 +13,59 @@ app.config["SQLALCHEMY_ECHO"] = True
 
 db.init_app(app)
 with app.app_context():
-    db.create_all()
+  db.create_all()
 
 
 # your routes here
 @app.route("/", methods=["GET"])
 def hello_world():
-    """
-    Endpoint for printing netid
-    """
-    return os.environ.get("NETID") + " was here!"
+  """
+  Endpoint for printing netid
+  """
+  return os.environ.get("NETID") + " was here!"
 
 
-@app.route("/api/courses/", methods=["GET"])
-def get_courses():
-    """
-    Endpoint for getting all courses
-    """
-    courses = [t.serialize() for t in Course.query.all()]
-    return json.dumps({"courses": courses}), 200
-
-
-@app.route("/api/courses/", methods=["POST"])
-def post_course():
-    """
-    Endpoint for posting a course
-    """
-    body = json.loads(request.data)
-    try:
-        new_course = Course(
-            code=body["code"],
-            name=body["name"]
-        )
-    except KeyError:
-        return json.dumps({"error": "Missing fields"}), 400
-
-    db.session.add(new_course)
-    db.session.commit()
-    return json.dumps(new_course.serialize()), 201
-
-
-@app.route("/api/courses/<int:course_id>/", methods=["GET"])
-def get_course(course_id):
-    """
-    Endpoint for getting a course
-    """
-    course = Course.query.filter_by(id=course_id).first()
-    if course is None:
-        return json.dumps({"error": "Course Not Found"}), 404
-    return json.dumps(course.serialize()), 200
-
-
-@app.route("/api/courses/<int:course_id>/", methods=["DELETE"])
-def delete_course(course_id):
-    """
-    Endpoint for deleting a course
-    """
-    course = Course.query.filter_by(id=course_id).first()
-    if course is None:
-        return json.dumps({"error": "Course Not Found"}), 404
-    db.session.delete(course)
-    db.session.commit()
-    return json.dumps(course.serialize()), 200
+@app.route("/api/users/", methods=["GET"])
+def get_users():
+  """
+  Endpoint for getting all courses
+  """
+  users = [t.serialize() for t in Users.query.all()]
+  return json.dumps({"users": users}), 200
 
 
 @app.route("/api/users/", methods=["POST"])
 def post_user():
-    """
-    Endpoint for creating an user
-    """
-    body = json.loads(request.data)
-    try:
-        new_user = Users(
-            name=body["name"],
-            netid=body["netid"]
-        )
-    except KeyError:
-        return json.dumps({"error": "Missing fields"}), 400
+  """
+  Endpoint for posting a course
+  """
+  body = json.loads(request.data)
+  try:
+    new_user = Users(
+      college=body["college"],
+      name=body["name"],
+      identifer=body["identifer"]
+    )
+  except KeyError:
+    return json.dumps({"error": "Missing fields"}), 400
 
-    db.session.add(new_user)
-    db.session.commit()
-    return json.dumps(new_user.serialize()), 201
+  db.session.add(new_user)
+  db.session.commit()
+  return json.dumps(new_user.serialize(), 201)
 
 
-@app.route("/api/users/<int:user_id>/", methods=["GET"])
-def get_user(user_id):
-    """
-    Endpoint for getting an user
-    """
-    user = Users.query.filter_by(id=user_id).first()
+@app.route("/api/users/tree/", methods=["POST"])
+def update_tree():
+  """
+  Endpoint for updating the tree amount of an user
+  """
+  body = json.loads(request.data)
+  try:
+    user = Users.query.filter_by(identifier=body["sub"]).first()
     if user is None:
-        return json.dumps({"error": "User Not Found"}), 404
-    return json.dumps(user.serialize()), 200
-
-
-@app.route("/api/courses/<int:course_id>/add/", methods=["POST"])
-def add_user(course_id):
-    """
-    Endpoint for adding users to course
-    """
-    body = json.loads(request.data)
-    try:
-        user_id = body["user_id"]
-        type = body["type"]
-    except KeyError:
-        return json.dumps({"error": "Missing fields"}), 400
-    course = Course.query.filter_by(id=course_id).first()
-    user = Users.query.filter_by(id=user_id).first()
-    if user is None or course is None:
-        return json.dumps({"error": "Course/User Not Found"}), 404
-    if type == "instructor":
-        course.instructors.append(user)
-        user.course_taught.append(course)
-    elif type == "student":
-        course.students.append(user)
-        user.courses.append(course)
-    else:
-        return json.dumps({"error": "Invalid Type"}), 400
-    db.session.commit()
-    return json.dumps(course.serialize()), 200
-
-
-@app.route("/api/courses/<int:course_id>/assignment/", methods=["POST"])
-def add_assignment(course_id):
-    """
-    Endpoint for creating assignments
-    """
-    body = json.loads(request.data)
-    course = Course.query.filter_by(id=course_id).first()
-    if course is None:
-        return json.dumps({"error": "course Not Found"}), 404
-    try:
-        new_assign = Assignments(
-            title=body["title"],
-            duedate=body["due_date"],
-            course_id=course_id
-        )
-    except KeyError:
-        return json.dumps({"error": "Missing fields"}), 400
-    db.session.add(new_assign)
-    db.session.commit()
-    return json.dumps(new_assign.serialize()), 201
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+      return json.dumps({"error": "User not found"}), 404
+    user.treenum = user.treenum + body["amount"]
+  except KeyError:
+    return json.dumps({"error": "Missing fields"}), 400
+  db.session.commit()
+  return json.dumps(user.serialize()), 201
--- src/db.py
+++ src/db.py
@@ -2,130 +2,37 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# your classes here
-
-# assoc between instructors and courses
-assoc_table = db.Table("assoc", db.Model.metadata,
-                       db.Column("user", db.Integer,
-                                 db.ForeignKey("users.id")),
-                       db.Column("course", db.Integer,
-                                 db.ForeignKey("courses.id"))
-                       )
-
-# assoc between students and courses
-assoc_table2 = db.Table("assoc2", db.Model.metadata,
-                        db.Column("user", db.Integer,
-                                  db.ForeignKey("users.id")),
-                        db.Column("course", db.Integer,
-                                  db.ForeignKey("courses.id"))
-                        )
-
-
-class Course(db.Model):
-    """
-    Table for courses
-    """
-    __tablename__ = "courses"
-    id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.String, nullable=False)
-    name = db.Column(db.String, nullable=False)
-    assignments = db.relationship(
-        "Assignments", cascade="delete")
-    instructors = db.relationship(
-        "Users", secondary=assoc_table, back_populates='course_taught')
-    students = db.relationship(
-        "Users", secondary=assoc_table2, back_populates='courses')
-
-    # instantiate a course item
-    def __init__(self, **kwargs):
-        self.code = kwargs.get("code")
-        self.name = kwargs.get("name")
-        self.assignments = []
-        self.instructors = []
-        self.students = []
-
-    # serialize a course item
-    def serialize(self):
-        return {
-            "id": self.id,
-            "code": self.code,
-            "name": self.name,
-            "assignments": [a.serialize_info() for a in self.assignments],
-            "instructors": [i.serialize_info() for i in self.instructors],
-            "students": [s.serialize_info() for s in self.students]
-        }
-
-    # serialize a course item with only basic info
-    def serialize_info(self):
-        return {
-            "id": self.id,
-            "code": self.code,
-            "name": self.name
-        }
-
 
 class Users(db.Model):
-    """
-    Table for users
-    """
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    netid = db.Column(db.String, nullable=False)
-    courses = db.relationship(
-        "Course", secondary=assoc_table2, back_populates='students')
-    course_taught = db.relationship(
-        "Course", secondary=assoc_table, back_populates='instructors')
+  """
+  Table for users
+  """
+  __tablename__ = "users"
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String, nullable=False)
+  identfier = db.Column(db.String, nullable=False)
+  college = db.Column(db.String, nullable=False)
+  treenum = db.Column(db.Float, nullable=False)
 
-    # instantiate a user
-    def __init__(self, **kwargs):
-        self.name = kwargs.get("name")
-        self.netid = kwargs.get("netid")
-        self.courses = []
-        self.course_taught = []
+  # instantiate a user
+  def __init__(self, **kwargs):
+    self.name = kwargs.get("name")
+    self.college = kwargs.get("college")
+    self.treenum = 0.0
+    self.identifer = kwargs.get("identifer")
 
-    # serialize a user
-    def serialize(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "netid": self.netid,
-            "courses": [c.serialize_info() for c in self.courses] + [c.serialize_info() for c in self.course_taught]
-        }
+  # serialize a user
+  def serialize(self):
+    return {
+      "id": self.id,
+      "name": self.name,
+      "college": self.college,
+      "treenum": self.treenum
+    }
 
-    # serialize a user without course field
-    def serialize_info(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "netid": self.netid,
-        }
-
-
-class Assignments(db.Model):
-    """
-    Table for assignment
-    """
-    __tablename__ = "assignments"
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    duedate = db.Column(db.Integer, nullable=False)
-    course_id = db.Column(db.Integer, db.ForeignKey(
-        "courses.id"), nullable=False)
-
-    # serialize an assignment
-    def serialize(self):
-        return {
-            "id": self.id,
-            "title": self.title,
-            "due_date": self.duedate,
-            "course": Course.query.filter_by(id=self.course_id).first().serialize_info()
-        }
-
-    # serialize an assignment without course info
-    def serialize_info(self):
-        return {
-            "id": self.id,
-            "title": self.title,
-            "due_date": self.duedate
-        }
+  # serialize a user without course field
+  def serialize_info(self):
+    return {
+      "name": self.name,
+      "netid": self.netid,
+    }
