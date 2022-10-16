6051ef43046115cfcf50f0d43ea63069616b5cac
fl327 <soxehli@gmail.com>
2022-10-16 00:45:19 -0400

    add canvas

--- TreeAppFront/screens/TabThreeScreen.tsx
+++ TreeAppFront/screens/TabThreeScreen.tsx
@@ -1,13 +1,13 @@
 import { StyleSheet, SafeAreaView, Button, Alert } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
-// import { Text, View } from '../components/Themed';
+import { Text, View } from '../components/Themed';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 import * as React from 'react';
 import * as WebBrowser from 'expo-web-browser';
 import * as Google from 'expo-auth-session/providers/google';
-import { Image, Text, View } from 'react-native';
-// import { Text, View } from './components/Themed';
+import { Image, Pressable, ActionSheetIOS } from 'react-native';
 
 
 export default function TabThreeScreen() {
@@ -15,8 +15,21 @@ export default function TabThreeScreen() {
     expoClientId: '752731696718-nhibeghj1u04eie23isujls8893l0sfi.apps.googleusercontent.com',
   });
 
-  const [accessToken, setAccessToken] = React.useState();
+  const [accessToken, setAccessToken] = React.useState(null);
   const [userInfo, setUserInfo] = React.useState();
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState(null);
+  const [items, setItems] = React.useState([
+    { label: 'College of Agriculture and Life Sciences', value: 'CALS' },
+    { label: 'College of Architecture, Art and Planning', value: 'CAAP' },
+    { label: 'College of Arts and Sciences', value: 'CAS' },
+    { label: 'Ann S. Bowers College of Computing and information science', value: 'COB' },
+    { label: 'SC Johnson College of Business', value: 'CBA' },
+    { label: 'College of Engineering', value: 'COE' },
+    { label: 'College of Human Ecology', value: 'CHE' },
+    { label: 'School of Hotel Administration', value: 'SHA' },
+    { label: 'School of Industrial and Labor Relations', value: 'ILR' },
+  ]);
 
   React.useEffect(() => {
     if (response?.type === 'success') {
@@ -40,12 +53,35 @@ export default function TabThreeScreen() {
       return (
         <View style={styles.userInfo}>
           <Image source={{ uri: userInfo.picture }} style={styles.profilePic} />
-          <Text>Welcome {userInfo.name}</Text>
+          <Text style={styles.text}>{userInfo.name}</Text>
+          <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
           <Text>{userInfo.email}</Text>
           <Text>{userInfo.id}</Text>
         </View>
       );
     }
+    else
+      return (
+        <View style={styles.userInfo}>
+          <DropDownPicker
+            style={styles.dropDown}
+            open={open}
+            value={value}
+            items={items}
+            setOpen={setOpen}
+            setValue={setValue}
+            setItems={setItems}
+          />
+          <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+          <Button
+            title='Sign in with Google'
+            disabled={!request || userInfo}
+            onPress={accessToken ? getUserData : () => {
+              promptAsync({ showInRecents: true });
+            }}
+          />
+        </View>
+      );
   }
 
   return (
@@ -56,13 +92,7 @@ export default function TabThreeScreen() {
           onPress={() => Alert.alert('Simple Button pressed')}
         /> */}
         {showUserInfo()}
-        <Button
-          disabled={!request || userInfo}
-          title="Login"
-          onPress={accessToken ? getUserData : () => {
-            promptAsync({ showInRecents: true });
-          }}
-        />
+
         {/* <Text style={styles.title}>Me</Text>
         <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
         <EditScreenInfo path="/screens/TabThreeScreen.tsx" /> */}
@@ -72,10 +102,27 @@ export default function TabThreeScreen() {
 }
 
 const styles = StyleSheet.create({
+  dropDown: {
+    width: 300,
+    marginVertical: 10,
+  },
+  text: {
+    fontSize: 20,
+    paddingVertical: 15
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 16,
+    paddingHorizontal: 50,
+    borderRadius: 5,
+    elevation: 3,
+  },
   container: {
     flex: 1,
     alignItems: 'center',
-    // justifyContent: 'center',
+    //justifyContent: 'stretch',
+    marginTop: 50,
     marginHorizontal: 16,
   },
   title: {
@@ -85,14 +132,18 @@ const styles = StyleSheet.create({
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: '180%',
   },
   userInfo: {
     alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 6,
+    marginTop: 50,
+    marginHorizontal: 16,
   },
   profilePic: {
     width: 50,
-    height: 50
+    height: 50,
+    borderRadius: 5,
   }
 });
--- TreeAppFront/screens/TabTwoScreen.tsx
+++ TreeAppFront/screens/TabTwoScreen.tsx
@@ -1,21 +1,48 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet, Alert, SafeAreaView } from 'react-native';
+import React, { useRef } from 'react';
+import { Asset } from 'expo-asset';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import Canvas, { Image as CanvasImage } from 'react-native-canvas';
+
+
 
 export default function TabTwoScreen() {
+  // const asset = Asset.fromModule(require('../assets/images/full-tree-1.png'));
+
+  const handleCanvas = (canvas) => {
+    const ctx = canvas.getContext('2d');
+    // ctx.fillStyle = 'red';
+    // ctx.fillRect(0, 0, 100, 100);
+
+
+    const img = new CanvasImage(canvas);
+    img.src = 'https://cdn.atcg.cc/full-tree-1.png';
+    img.addEventListener('load', () => {
+      ctx.drawImage(img, 0, 0, 100, 100);
+    });
+    // ctx.drawImage(img, 10, 10, 1000, 1000);
+    // img.onload = () => {
+    //   ctx.drawImage(img, 10, 10, 1000, 1000);
+    // }
+  };
+
   return (
+    // <Canvas ref={handleCanvas} style={{ width: '100%', height: '100%' }} />
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+      <Text >Tab Two</Text>
+      <Canvas ref={handleCanvas} style={{ width: '100%', height: '100%' }} />
+      {/* <View style={styles.separator}s lightColor="#eee" darkColor="rgba(255,255,255,0.1)" /> */}
+      {/* <EditScreenInfo path="/screens/TabTwoScreen.tsx" /> */}
     </View>
   );
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 2,
     alignItems: 'center',
     justifyContent: 'center',
   },
